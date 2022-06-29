f99848163646110c59278a7434b6dafd8e2a86a0
adichakra <ace.chakra@gmail.com>
2022-06-29 16:12:43 -0500

    Finished Button Clicker

---
+++ Web_Fundamentals/Week_2/Day_2/script.js
@@ -0,0 +1,11 @@
+function hide(element){
+    element.remove();
+}
+
+function logout(element){
+    if (element.innerText == "Login"){
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+    }
+}
